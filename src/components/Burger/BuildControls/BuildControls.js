5fc391f058c0ddaf5f1bfc876c9a8260c82382d4
AlexTicovschi <alexmticovschi@gmail.com>
2018-03-30 19:24:38 +0100

    addding the BuildControl component

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import BuildControl from './BuildControl/BuildControl'
+
+const buildControls = (props) => (
+    <div>
+            
+    </div>
+);
+
+
+export default buildControls;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
     state = {
@@ -16,7 +17,7 @@ class BurgerBuilder extends Component {
         return (
             <Fragment>
                 <Burger ingredients={this.state.ingredients}/>
-                <div>Build Controls</div>
+                <BuildControls />
             </Fragment>
         )
     }
