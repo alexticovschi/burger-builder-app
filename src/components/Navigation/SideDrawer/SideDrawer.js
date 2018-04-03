e33e5e1d94ae6a03417e73856b0b3ed4780c82f8
AlexTicovschi <alexmticovschi@gmail.com>
2018-04-03 10:39:49 +0100

    working on responsive adjustments

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -8,7 +8,9 @@ const sideDrawer = (props) => {
     //...
     return (
         <div className={classes.SideDrawer}>
-            <Logo/>
+            <div className={classes.Logo}>
+                <Logo/>
+            </div>           
             <nav>
                 <NavigationItems/>
             </nav>
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -7,8 +7,10 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <Logo/>
         <div>MENU</div>
+        <div className={classes.Logo}>
+            <Logo/>
+        </div>    
         <nav>
             <NavigationItems/>
         </nav>
