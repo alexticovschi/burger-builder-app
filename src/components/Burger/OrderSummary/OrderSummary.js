a9d75e2d3f28ca9a7f8492a13d2d500a7bf5457b
AlexTicovschi <alexmticovschi@gmail.com>
2018-04-06 10:47:35 +0100

    improving performance

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,29 +1,35 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(ingKey => {
-            return (
-                <li key={ingKey}>
-                    <span style={{textTransform:'capitalize'}}>{ingKey}</span>: {props.ingredients[ingKey]}
-                </li>
-            );
+class OrderSummary extends Component {
+    componentWillUpdate() {
+        console.log('[Order Summary] WillUpdate');
+    }
+
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map(ingKey => {
+                return (
+                    <li key={ingKey}>
+                        <span style={{textTransform:'capitalize'}}>{ingKey}</span>: {this.props.ingredients[ingKey]}
+                    </li>
+                );
         });
-    return (
-        <Fragment>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-        </Fragment>
-    )
+        return (
+            <Fragment>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </Fragment>
+        )
+    }
 }
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,34 @@
-import React,{ Fragment } from 'react';
+import React,{ Component, Fragment } from 'react';
 
 import classes from './Modal.css';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-    <Fragment>
-        <Backdrop show={props.show} clicked={props.modalClosed}/>
-        <div 
-            className={classes.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-            }}
-            >
-            {props.children}
-        </div>
-    </Fragment>
-)
+class Modal extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        // update this component only if 'show' changes
+        return nextProps.show !== this.props.show;
+    }
+
+    componentWillUpdate() {
+        console.log('[Modal] WillUpdate');
+    }
+
+    render () {
+        return (
+            <Fragment>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+                <div 
+                    className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}
+                    >
+                    {this.props.children}
+                </div>
+            </Fragment>
+        )
+    }
+}
  
-export default modal;
+export default Modal;
