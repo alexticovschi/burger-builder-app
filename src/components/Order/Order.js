61e845be70b18707da7794a606488e3c9ab67772
AlexTicovschi <alexmticovschi@gmail.com>
2018-05-30 23:27:13 +0100

    fixing the displayed price

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -30,7 +30,7 @@ const order = (props) => {
     return (
         <div className={classes.Order}>
             <p>Ingredients: {ingredientOutput}</p>
-            <p>Price: <strong>USD {props.price}</strong></p>
+            <p>Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong></p>
         </div>
     );
 };
