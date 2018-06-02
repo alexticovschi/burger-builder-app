a6e0e45d0db5abf32a8c7f625ad38a6b8ac26671
AlexTicovschi <alexmticovschi@gmail.com>
2018-06-02 14:42:11 +0100

    handling form submission

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -58,7 +58,7 @@ class ContactData extends Component {
                         {value: 'cheapest', displayValue: 'Cheapest'}                        
                     ]
                 },
-                value: ''
+                value: 'fastest'
             }
         },
         loading: false
@@ -68,9 +68,14 @@ class ContactData extends Component {
         event.preventDefault();
         // the request is about to get sent so set loading to true
         this.setState({ loading: true });
+        const formData = {};
+        for (let formElementIdentifier in this.state.orderForm) {
+            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+        }
         const order = {
             ingredients: this.props.ingredients,
-            price: this.props.price
+            price: this.props.price,
+            orderData: formData
         }
         axios.post('/orders.json', order)
             .then(response => {
@@ -116,7 +121,7 @@ class ContactData extends Component {
         console.log('formElementsArray: ', formElementsArray);
 
         let form = (
-            <form>
+            <form onSubmit={this.orderHandler}>
                 {/* {formElementsArray} */}
                 {formElementsArray.map(formElement => (
                     <Input 
@@ -126,7 +131,7 @@ class ContactData extends Component {
                         value={formElement.config.value}
                         changed={(event) => this.inputChangedHandler(event, formElement.id)} />
                 ))}                                                  
-                <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+                <Button btnType="Success">ORDER</Button>
             </form>
         );
         if (this.state.loading) {
