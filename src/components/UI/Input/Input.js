3bd89c1e4170ff231c70fa2e809fd4bd08575f19
AlexTicovschi <alexmticovschi@gmail.com>
2018-06-03 14:22:33 +0100

    showing error messages

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -47,10 +47,16 @@ const input = (props) => {
                 onChange={props.changed} />;
     }
 
+    let validationError = null;
+    if (props.invalid && props.touched) {
+        validationError = <p className={classes.ValidationError}>Please enter a valid value!</p>
+    }
+
     return (
         <div className={classes.Input}>
             <label className={classes.Label}>{props.label}</label>
             {inputElement}
+            {validationError}
         </div>
     );
 }
