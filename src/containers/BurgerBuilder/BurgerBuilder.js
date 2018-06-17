328d8d9685c9f6f6a9b5dce7ce053886adbff0b9
AlexTicovschi <alexmticovschi@gmail.com>
2018-06-17 17:09:10 +0100

    finishing the reducer for ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -27,13 +27,13 @@ class BurgerBuilder extends Component {
 
     componentDidMount() {
         // console.log(this.props);
-        axios.get('https://react-burger-builder-d41a0.firebaseio.com/ingredients.json')
-            .then(response => {
-                this.setState({ ingredients: response.data})
-            })
-            .catch(error => {
-                this.setState({ error: true })
-            })
+        // axios.get('https://react-burger-builder-d41a0.firebaseio.com/ingredients.json')
+        //     .then(response => {
+        //         this.setState({ ingredients: response.data})
+        //     })
+        //     .catch(error => {
+        //         this.setState({ error: true })
+        //     })
     }
 
     purchaseHandler = () => {
