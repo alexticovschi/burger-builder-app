c950bd37c5be2b82bf0347d9130cda7a27e5ae8d
AlexTicovschi <alexmticovschi@gmail.com>
2018-06-22 11:47:05 +0100

    updating the total price

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -10,12 +10,6 @@ import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import axios from '../../axios-orders';
 import * as actionTypes from '../../store/actions';
 
-const INGREDIENT_PRICES = {
-    salad: 0.5,
-    cheese: 0.4,
-    meat: 1.3,
-    bacon: 0.6
-}
 
 class BurgerBuilder extends Component {
     state = {
@@ -71,57 +65,57 @@ class BurgerBuilder extends Component {
         this.setState({ purchasable: sum > 0 });
     }
 
-    addIngredientHandler = (type) => {
-        // check the old count of the ingredient
-        const oldCount = this.state.ingredients[type];
-        const updatedCount = oldCount + 1;
+    // addIngredientHandler = (type) => {
+    //     // check the old count of the ingredient
+    //     const oldCount = this.state.ingredients[type];
+    //     const updatedCount = oldCount + 1;
 
-        // create a copy of ingredients object
-        const updatedIngredients = {...this.state.ingredients};
+    //     // create a copy of ingredients object
+    //     const updatedIngredients = {...this.state.ingredients};
         
-        // Take the updatedIngredients object, access the type for which I have to
-        //   update the ingredients and set the amount of the ingredients equal to updatedCount
-        updatedIngredients[type] = updatedCount;
+    //     // Take the updatedIngredients object, access the type for which I have to
+    //     //   update the ingredients and set the amount of the ingredients equal to updatedCount
+    //     updatedIngredients[type] = updatedCount;
 
-        const priceAddition = INGREDIENT_PRICES[type];
-        const oldPrice = this.state.totalPrice;
-        const newPrice = oldPrice + priceAddition;
+    //     const priceAddition = INGREDIENT_PRICES[type];
+    //     const oldPrice = this.state.totalPrice;
+    //     const newPrice = oldPrice + priceAddition;
 
-        // updade the state to have new ingredients and new price
-        this.setState({
-            totalPrice: newPrice,
-            ingredients: updatedIngredients
-        })
-        this.updatePurchaseState(updatedIngredients);
-        console.log('updatedIngredients:',updatedIngredients)
-    }
+    //     // updade the state to have new ingredients and new price
+    //     this.setState({
+    //         totalPrice: newPrice,
+    //         ingredients: updatedIngredients
+    //     })
+    //     this.updatePurchaseState(updatedIngredients);
+    //     console.log('updatedIngredients:',updatedIngredients)
+    // }
 
-    removeIngredientHandler = (type) => {
-        // check the old count of the ingredient
-        const oldCount = this.state.ingredients[type];
+    // removeIngredientHandler = (type) => {
+    //     // check the old count of the ingredient
+    //     const oldCount = this.state.ingredients[type];
 
-        if(oldCount === 0) return;
-        const updatedCount = oldCount - 1;
+    //     if(oldCount === 0) return;
+    //     const updatedCount = oldCount - 1;
 
-        // create a copy of ingredients object
-        const updatedIngredients = {...this.state.ingredients};
+    //     // create a copy of ingredients object
+    //     const updatedIngredients = {...this.state.ingredients};
         
-        // Take the updatedIngredients object, access the type for which I have to
-        //   update the ingredients and set the amount of the ingredients equal to updatedCount
-        updatedIngredients[type] = updatedCount;
+    //     // Take the updatedIngredients object, access the type for which I have to
+    //     //   update the ingredients and set the amount of the ingredients equal to updatedCount
+    //     updatedIngredients[type] = updatedCount;
 
-        const priceDeduction = INGREDIENT_PRICES[type];
-        const oldPrice = this.state.totalPrice;
-        const newPrice = oldPrice - priceDeduction;
+    //     const priceDeduction = INGREDIENT_PRICES[type];
+    //     const oldPrice = this.state.totalPrice;
+    //     const newPrice = oldPrice - priceDeduction;
 
-        // updade the state to have new ingredients and new price
-        this.setState({
-            totalPrice: newPrice,
-            ingredients: updatedIngredients
-        })
-        this.updatePurchaseState(updatedIngredients);
-        console.log('updatedIngredients:',updatedIngredients)
-    }
+    //     // updade the state to have new ingredients and new price
+    //     this.setState({
+    //         totalPrice: newPrice,
+    //         ingredients: updatedIngredients
+    //     })
+    //     this.updatePurchaseState(updatedIngredients);
+    //     console.log('updatedIngredients:',updatedIngredients)
+    // }
 
     render () {
         // copy of ingredients object
@@ -144,13 +138,13 @@ class BurgerBuilder extends Component {
                         disabled={disabledInfo}
                         purchasable={this.state.purchasable}
                         ordered={this.purchaseHandler}
-                        price={this.state.totalPrice}
+                        price={this.props.price}
                     />
                 </Fragment>
             );
             orderSummary = <OrderSummary 
                 ingredients={this.props.ings}
-                price={this.state.totalPrice}
+                price={this.props.price}
                 purchaseCancelled={this.purchaseCancelHandler}
                 purchaseContinued={this.purchaseContinueHandler}/>;
         
@@ -172,7 +166,8 @@ class BurgerBuilder extends Component {
 
 const mapStateToProps = (state) => {
     return {
-        ings: state.ingredients
+        ings: state.ingredients,
+        price: state.totalPrice
     }
 }
 
