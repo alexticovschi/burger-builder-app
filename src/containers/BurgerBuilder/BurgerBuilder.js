6516ded3551bcfdc5e6251cb2cc2db22e86be932
AlexTicovschi <alexmticovschi@gmail.com>
2018-07-03 09:51:51 +0100

    executing asynchronous code

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -20,14 +20,14 @@ class BurgerBuilder extends Component {
     }
 
     componentDidMount() {
-        // console.log(this.props);
-        // axios.get('https://react-burger-builder-d41a0.firebaseio.com/ingredients.json')
-        //     .then(response => {
-        //         this.setState({ ingredients: response.data})
-        //     })
-        //     .catch(error => {
-        //         this.setState({ error: true })
-        //     })
+        console.log(this.props);
+        axios.get('https://react-burger-builder-d41a0.firebaseio.com/ingredients.json')
+            .then(response => {
+                this.setState({ ingredients: response.data})
+            })
+            .catch(error => {
+                this.setState({ error: true })
+            })
     }
 
     purchaseHandler = () => {
